f9beef8c8afc269d47cf3580941b2df1cdb50750
herve perel <herveperel@gmail.com>
2023-09-17 16:08:44 +0200

    props.1

--- new-app/src/App.jsx
+++ new-app/src/App.jsx
@@ -5,11 +5,23 @@ import './App.css'
 import MyTitle from "./components/MyTitle"
 import PokemonCard from "./components/PokemonCard"
 
-
+ 
 function App() {
+ const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "new",
+  },
+];
+
+
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]}/>
     </div>
   );
 }
